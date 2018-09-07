bbd625acd2a4b163c280f3024b8dddb6d60ac149
Amal Abeygunawardana <amal.abeygunawardana@team.telstra.com>
2018-09-07 16:10:17 +1000

    Works with office ui fabric react components

---
+++ app/components/NavBar.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Nav, INavProps } from 'office-ui-fabric-react/lib/Nav';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="ms-NavExample-LeftPane">
+        <Nav
+          groups={[
+            {
+              links: [
+                { name: 'Documents', url: 'http://example.com', key: 'key3', isExpanded: true },
+                { name: 'Pages', url: 'http://msn.com', key: 'key4' },
+                { name: 'Notebook', url: 'http://msn.com', key: 'key5' },
+                { name: 'Long Name Test for ellipse', url: 'http://msn.com', key: 'key6' },
+                {
+                  name: 'AddFavorite',
+                  url: 'http://cnn.com',
+                  onClick: this._onClickHandler2,
+                  icon: 'AddFavorite',
+                  key: 'key8'
+                },
+                {
+                  name: 'BIDashboard',
+                  url: 'http://cnn.com',
+                  onClick: this._onClickHandler2,
+                  iconProps: { iconName: 'BIDashboard' },
+                  key: 'key9'
+                }
+              ]
+            }
+          ]}
+          expandedStateText={'expanded'}
+          collapsedStateText={'collapsed'}
+          selectedKey={'key3'}
+        />
+      </div>
+    );
+  };
+
+  _onClickHandler(e: React.MouseEvent<HTMLElement>): false {
+    alert('test');
+    return false;
+  }
+
+  _onClickHandler2(e: React.MouseEvent<HTMLElement>): false {
+    return false;
+  }
+}
+export default NavBar;
--- app/entry.js
+++ app/entry.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
 import Hello from './components/Hello';
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   render() {
     return (
-      <Hello />
+      <NavBar />
     )
   }
 }
